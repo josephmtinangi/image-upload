4cf1cdf3c31261aee44c80e6180ab14239e7f6b5
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-09-20 11:26:11 +0000

    Configure database

---
+++ database/migrations/20170920112042_create_images_table.php
@@ -0,0 +1,44 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateImagesTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up()
+    {
+        $images = $this->table('images');
+
+        $images->addColumn('hash', 'string')
+              ->addColumn('url', 'string')
+              ->addColumn('created_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+              ->addColumn('updated_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
+              ->save();
+    }
+
+    public function down()
+    {
+        $this->dropTable('images');
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use AI\Models\Image;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application;
@@ -10,7 +12,26 @@ $app->register(new Silex\Provider\TwigServiceProvider, [
 	'twig.path' => __DIR__ . '/../views'
 ]);
 
+$app->register(new Silex\Provider\DoctrineServiceProvider, [
+	'db.options' => [
+		'driver'   => 'pdo_mysql',
+		'host'	   => 'localhost',
+		'dbname'   => 'image_upload',
+		'user'     => 'homestead',
+		'password' => 'secret',
+		'charset'  => 'utf8',
+	],
+]);
+
 $app->get('/', function () use ($app) {
+
+	$images = $app['db']->prepare("SELECT * FROM images");
+	$images->execute();
+
+	$images = $images->fetchAll(PDO::FETCH_CLASS, Image::class);
+
+	var_dump($images);die();
+
 	return $app['twig']->render('home.twig');
 });
 
